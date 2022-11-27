1dc8d070b3039ea8a0aa4afdb9cf34acaccc5f90
lwd <1850245174@qq.com>
2022-11-27 15:16:11 +0800

    easfcdsd

--- vue02/src/App.vue
+++ vue02/src/App.vue
@@ -5,21 +5,21 @@
 </template>
 
 <style lang="scss">
+
 @import './assets/scss/reset.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
 nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
 
     &.router-link-exact-active {
       color: #42b983;
