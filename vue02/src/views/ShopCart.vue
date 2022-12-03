bb1a1d6c59f2f02c7dd16038a4f365a20f047a82
jzh <1499765600@qq.com>
2022-12-03 21:17:04 +0800

    购物车

--- vue02/src/views/ShopCart.vue
+++ vue02/src/views/ShopCart.vue
@@ -8,10 +8,10 @@
       @selection-change="handleSelectionChange"
     >
       <el-table-column type="selection" width="55" />
-      <el-table-column property="goodsId" label="商品ID" width="120" />
-      <el-table-column property="num" label="商品数" show-overflow-tooltip />
-      <el-table-column property="num" label="商品单价" show-overflow-tooltip />
-      <el-table-column property="num" label="总价" show-overflow-tooltip />
+      <el-table-column property="goodsVo.goods.name" label="商品" width="120" />
+      <el-table-column property="goodsVo.goods.price" label="商品单价" show-overflow-tooltip />
+      <el-table-column property="shop.num" label="商品数" show-overflow-tooltip />
+      <el-table-column  property="shop.num" label="总价" show-overflow-tooltip />
       <el-table-column label="选择" >
         <template #default="scope">
           <el-button @click="console.log(scope.row)">详细</el-button>
@@ -32,55 +32,11 @@ export default {
   data() {
     return {
       multipleSelection: null,
-      resList : null,
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-08',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-06',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-07',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-      ],
-
+      resList: null,
+      sum: null,
     }
   },
   mounted() {
-    const internalInstance = getCurrentInstance(); // 有效
-    console.log(internalInstance);
-    let ultipleTabInstance = toRefs(internalInstance.refs.multipleTableRef);
-    console.log(ultipleTabInstance);
-    this.tableData.forEach(row => {
-      ultipleTabInstance.toggleRowSelection.value(row, undefined);
-    })
     let url = "shop/list"
     let data = new FormData();
     let tokenx = this.$cookies.get("token");
@@ -97,15 +53,31 @@ export default {
     axios.get(url,config).then(res=>{
       console.log(res);
       let tmp = [];
-      let Myreslist = res.data.data.shopList;
+      let price = 0;
+
+      let Myreslist = res.data.data.shopVoList;
       let len = Myreslist.length;
       for (let i = 0; i < len; i++) {
-        tmp.push(Myreslist[i]);
+        let ttmp = [];
+        ttmp.push(Myreslist[i].shop);
+        price = Myreslist[i].goodsVo.goods.price * Myreslist[i].shop.num;
+        ttmp.push({"sum": price});
+        tmp.push(ttmp);
       }
       this.resList = tmp;
+      console.log(tmp)
     }).catch(err=>{
       console.log(err);
     })
+
+    // const internalInstance = getCurrentInstance(); // 有效
+    // console.log(internalInstance);
+    // let ultipleTabInstance = toRefs(internalInstance.refs.multipleTableRef);
+    // console.log(ultipleTabInstance);
+    // this.tableData.forEach(row => {
+    //   ultipleTabInstance.toggleRowSelection.value(row, undefined);
+    // })
+
   },
   methods: {
     handleSelectionChange(val) {
